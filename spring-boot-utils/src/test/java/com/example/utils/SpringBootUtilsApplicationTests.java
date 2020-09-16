472b4f4a7c8df3630e6d885cf13b048e6568ac84
shizeying <w741069229@gmail.com>
2020-09-16 21:36:46 +0800

    utils新增方法
    Signed-off-by: shizeying <w741069229@gmail.com>

--- spring-boot-utils/src/test/java/com/example/utils/SpringBootUtilsApplicationTests.java
+++ spring-boot-utils/src/test/java/com/example/utils/SpringBootUtilsApplicationTests.java
@@ -1,5 +1,8 @@
 package com.example.utils;
 
+import com.google.common.base.Stopwatch;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +12,16 @@ class SpringBootUtilsApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	public int removeDuplicates(int[] nums) {
+		
+		Long count = Stream.of(nums).distinct().count();
+		return count.intValue();
+	}
+  public static void main(String[] args) {
+		//
+		Stopwatch stopwatch = Stopwatch.createStarted();
+// do something
+		long second = stopwatch.elapsed(TimeUnit.SECONDS);
+    System.out.println(second);
+  }
 }
