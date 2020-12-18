48daf89ef1d2b2ed68d5e03d606fd94c27c3c114
shizeying <w741069229@gmail.com>
2020-12-18 21:36:49 +0800

    auto code

---
+++ word-transform-pdf-start/src/main/java/com/example/transform/WordTransformPdfStartApplication.java
@@ -0,0 +1,13 @@
+package com.example.transform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordTransformPdfStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordTransformPdfStartApplication.class, args);
+	}
+
+}
---
+++ word-transform-pdf-start/src/test/java/com/example/transform/WordTransformPdfStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordTransformPdfStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
