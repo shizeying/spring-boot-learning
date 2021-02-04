c40db3dea248609f343283a3f2cda947b464e014
shizeying <w741069229@gmail.com>
2021-02-04 21:04:07 +0800

    aa

---
+++ spring-boot-large-file-download/src/main/java/com/example/SpringBootLargeFileDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLargeFileDownloadApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLargeFileDownloadApplication.class, args);
+	}
+	
+}
