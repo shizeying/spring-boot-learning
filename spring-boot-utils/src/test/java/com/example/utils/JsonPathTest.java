69de8aa7684b14fe86ed0283f8e25d8a3820021e
shizeying <w741069229@gmail.com>
2022-07-09 22:07:33 +0800

    add: diff工具类新增

--- spring-boot-utils/src/test/java/com/example/utils/JsonPathTest.java
+++ spring-boot-utils/src/test/java/com/example/utils/JsonPathTest.java
@@ -1,5 +1,6 @@
 package com.example.utils;
-
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 /**
  * https://github.com/json-path/JsonPath
  *
@@ -8,4 +9,12 @@ package com.example.utils;
  */
 public class JsonPathTest {
 
+ public static void main(String args[]) {
+      BiMap<Integer, String> empIDNameMap = HashBiMap.create();
+      empIDNameMap.put(new Integer(101), "Mahesh");
+      empIDNameMap.put(new Integer(102), "Sohan");
+      empIDNameMap.put(new Integer(103), "Ramesh");
+      //Emp Id of Employee "Mahesh"
+      System.out.println(empIDNameMap.inverse().get("Mahesh"));
+   }
 }
